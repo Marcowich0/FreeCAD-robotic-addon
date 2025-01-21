ebc01be852f5c6e885f1661346973eeb4b801bf9
Marc <98757467+Marcowich0@users.noreply.github.com>
2025-01-21 22:39:36 +0100

    created seperate function for updating DH coordinate systems

--- RobotObject.py
+++ RobotObject.py
@@ -1,6 +1,7 @@
 import FreeCAD
 import FreeCADGui
 import os
+import re
 
 class RobotObject:
     def __init__(self, obj):
@@ -8,10 +9,12 @@ class RobotObject:
         obj.Proxy = self
         
         obj.addProperty("App::PropertyLinkList", "Constraints", "Robot", "List of links").Constraints = []
+        obj.addProperty("App::PropertyLinkList", "Bodies", "Robot", "List of bodies").Bodies = []
         obj.addProperty("App::PropertyLinkList", "CoordinateSystems", "Robot", "List of coordinate systems").CoordinateSystems = []
 
         obj.addProperty("App::PropertyString", "Type", "Base", "Type of the object").Type = "Robot"
         obj.setEditorMode("Type", 1)  # Make the property read-only
+        #obj.setEditorMode("Bodies", 1)  # Make the property read-only
         
         # Additional setup if needed
         self.Type = "Robot"
@@ -24,6 +27,7 @@ class RobotObject:
         """Handle property changes."""
         if prop == "Constraints":
             FreeCAD.Console.PrintMessage(f"Constraints updated: {obj.Constraints}\n")
+
 
 
 class RobotViewProvider:
@@ -107,5 +111,57 @@ def get_robot():
         if hasattr(obj, 'Type') and obj.Type == 'Robot':
             return obj
     return None
+
+
+def drawDanevitHartenberg(obj):
+    if not obj.CoordinateSystems:
+        """Draw the robot using the Denavit-Hartenberg parameters."""
+        lcs = FreeCAD.ActiveDocument.addObject( 'PartDesign::CoordinateSystem', f'LCS_link_{0}' )
+        FreeCAD.ActiveDocument.getObject("RobotContainer").addObject(lcs)
+    else:
+        lcs = obj.CoordinateSystems[0]
+
+    lcs.Placement.Rotation = FreeCAD.Rotation(FreeCAD.Vector(1,0,0), FreeCAD.Vector(0,1,0), FreeCAD.Vector(0,0,1))
+    lcs_arr = [lcs]
+
+        
+    for i, joint, body in zip(range(len(obj.Constraints)), obj.Constraints, obj.Bodies):
+        if not obj.CoordinateSystems:
+            lcs = FreeCAD.ActiveDocument.addObject( 'PartDesign::CoordinateSystem', f'LCS_link_{i+1}' ) # Adds coordinate system to the document
+        else:
+            lcs = obj.CoordinateSystems[i+1]
+
+        edge_nr = int(re.search(r'\d+$', joint.Reference1[1][0]).group()) - 1 # Finds edge number from reference
+
+        print(f"ref body: {body.Name}, ref edge nr {edge_nr}, joint name: {body.Name}") # debug
+
+        circle = FreeCAD.ActiveDocument.getObject(body.Name).Shape.Edges[edge_nr].Curve # Finds the circle of the constraint
+        lcs.Placement.Base = circle.Center # Sets the base of the coordinate system to the center of the circle
+        
+        last_x = lcs_arr[-1].Placement.Rotation.multVec(FreeCAD.Vector(1,0,0))
+        last_z = lcs_arr[-1].Placement.Rotation.multVec(FreeCAD.Vector(0,0,1))
+        
+        parallel = last_z.cross(circle.Axis).Length < 1e-6 # Checks if the circle axis is parallel to the last z-axis
+        print(f"parallel: {parallel}") # debug
+        if parallel:
+            z_axis = last_z
+            x_axis = last_x
+
+        else:
+            z_axis = circle.Axis # Sets the axis of the coordinate system to the axis of the circle
+            x_axis = last_z.cross(z_axis)
+
+        y_axis = z_axis.cross(x_axis)
+
+        lcs.Placement.Rotation = FreeCAD.Rotation(x_axis, y_axis, z_axis)
+
+        print(f"z_axis: {z_axis}, x_axis: {x_axis}, y_axis: {y_axis}") # debug
+        print(f"length of z_axis: {round(z_axis.Length, 3)}, length of x_axis: {round(x_axis.Length, 3)}, length of y_axis: {round(y_axis.Length, 3)}")
+        print(" -- ")
+        
+        FreeCAD.ActiveDocument.getObject("RobotContainer").addObject(lcs)
+        lcs_arr.append(lcs)
+
+    obj.CoordinateSystems = lcs_arr
 
 FreeCADGui.addCommand('CreateRobotCommand', CreateRobotCommand())
